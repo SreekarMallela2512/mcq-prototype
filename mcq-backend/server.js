01094ae47340f33a336999962f0b32b237eea0dd
SreekarMallela <sreekarmallela2512@gmail.com>
2025-09-04 01:37:33 +0530

    backendwork

--- mcq-backend/server.js
+++ mcq-backend/server.js
@@ -266,3 +266,6 @@ app.get('/api/test/history', authenticateToken, async (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
+app.get('/', (req, res) => {
+  res.status(200).send('API is running successfully!');
+});
